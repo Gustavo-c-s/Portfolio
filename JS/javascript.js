d211595fcf40e47255234a6c8b47c85f226de618
Gustavo santoos <124536279+Gustavo-c-s@users.noreply.github.com>
2024-01-25 23:50:30 -0300

    Portfolio
    criando um portfolio

---
+++ JS/javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".sobre").click(function () {
+        $("#mim-sobre").slideToggle(800);
+    });
+});
+
+$(document).ready(function () {
+    $(".pjt").click(function () {
+        $("#cont-pjt").slideToggle(800);
+    });
+});
+
+$(document).ready(function () {
+    $(".conh").click(function () {
+        $("#ra-conh").slideToggle(800);
+    });
+});
+
