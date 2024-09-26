c15da61e75588e59d4184d69f376ce789bd792b7
GustavoCS <gustatere1@gmail.com>
2024-09-26 17:39:50 -0300

    Atualizei

--- JS/javascript.js
+++ JS/javascript.js
@@ -5,3 +5,16 @@ function aumentarImagem(elemento) {
   function normalizarImagem(elemento) {
     elemento.style.transform = 'scale(1)';
   }
+  function showProject(event,projectNumber) {
+    event.preventDefault();
+    // Oculta todos os projetos
+    var projects = document.querySelectorAll('.box');
+    projects.forEach(function(project) {
+      project.style.display = 'none';
+    });
+  
+    // Mostra o projeto específico
+    var selectedProject = document.getElementById('project-' + projectNumber);
+    selectedProject.style.display = 'flex';
+  }
+  
